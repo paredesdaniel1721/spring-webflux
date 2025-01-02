64fd6e9d8abd8e8a98251b378a9e862ee6b36722
nilovila <nvila@mowa.com.pe>
2025-01-02 00:32:22 -0500

    Controller

--- spring-boot-webflux/src/main/java/pe/com/baseIdeas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux/src/main/java/pe/com/baseIdeas/springboot/webflux/app/controllers/ProductoController.java
@@ -3,6 +3,7 @@ package pe.com.baseIdeas.springboot.webflux.app.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import pe.com.baseIdeas.springboot.webflux.app.models.dao.ProductoDao;
 import pe.com.baseIdeas.springboot.webflux.app.models.documents.Producto;
 import reactor.core.publisher.Flux;
@@ -13,6 +14,7 @@ public class ProductoController {
     @Autowired
     private ProductoDao productoDao;
 
+    @GetMapping({"/listar", "/"})
     public String listar (Model model) {
         Flux<Producto> productos = productoDao.findAll();
 
