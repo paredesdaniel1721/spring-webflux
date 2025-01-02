48824308277589659ac389a316b8ae58ef6a6832
nilovila <nvila@mowa.com.pe>
2025-01-01 22:14:27 -0500

    first commit

---
+++ src/main/java/pe/com/baseIdeas/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -0,0 +1,41 @@
+package pe.com.baseIdeas.springboot.webflux.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import pe.com.baseIdeas.springboot.webflux.app.models.dao.ProductoDao;
+import pe.com.baseIdeas.springboot.webflux.app.models.documents.Producto;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootWebfluxApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProductoDao productoDao;
+	
+	private static Logger logger = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux.just(new Producto("TV Panasonic Pantalla LCD", 514.89),
+				new Producto("Sony Camara HD Digital", 132.99),
+				new Producto("Apple iPod", 54.79),
+				new Producto("Sony Notebook", 876.59),
+				new Producto("Dron Panasonic", 210.69),
+				new Producto("Bicicleta Montanera", 374.39),
+				new Producto("HP Impresora 345", 345.99),
+				new Producto("Laptop Dell 763", 763.39),
+				new Producto("Teclado logictec", 264.29))
+		.map(producto -> productoDao.save(producto))
+		.subscribe(i -> logger.info("Insert" + i.toString()));
+	}
+
+}
---
+++ src/test/java/pe/com/baseIdeas/springboot/webflux/app/SpringBootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.baseIdeas.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
